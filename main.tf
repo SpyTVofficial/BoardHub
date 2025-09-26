29edca3cf0e868703d0d31f6bed11b0651807f34
Ian Mores <imores@jamey.lu>
2025-09-26 12:00:34 +0000

    testing

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "hcloud_ssh_key" "default" {
 # -------------------------
 resource "hcloud_server" "k8s_master" {
   name        = "${var.cluster_name}-master"
-  image       = "ubuntu-22.04.5"
+  image       = "ubuntu-22.04"
   server_type = "cpx11"
   location    = "nbg1"
   ssh_keys    = [hcloud_ssh_key.default.id]
@@ -36,9 +36,8 @@ resource "hcloud_server" "k8s_master" {
 # Worker nodes
 # -------------------------
 resource "hcloud_server" "k8s_worker" {
-  count       = 2
   name        = "${var.cluster_name}-worker-${count.index + 1}"
-  image       = "ubuntu-22.04.5"
+  image       = "ubuntu-22.04"
   server_type = "cpx11"
   location    = "nbg1"
   ssh_keys    = [hcloud_ssh_key.default.id]
