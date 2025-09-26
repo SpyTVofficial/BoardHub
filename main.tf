ddefecef967f14e4d1efdd517bad6e0d9bfc8003
Ian Mores <imores@jamey.lu>
2025-09-26 09:42:36 +0000

    ssh key

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
-resource "hcloud_ssh_key" "default" {
-  name       = "github-actions-key"
-  public_key = file("${path.module}/id_rsa.pub")
+# Generate an SSH key using Terraform
+resource "tls_private_key" "default" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
+# Use the generated public key in Hetzner
+resource "hcloud_ssh_key" "default" {
+  name       = "terraform-generated-key"
+  public_key = tls_private_key.default.public_key_openssh
+}
+
+# Hetzner master node
 resource "hcloud_server" "k8s_master" {
   name        = "${var.cluster_name}-master"
   image       = "ubuntu-22.04"
@@ -11,6 +19,7 @@ resource "hcloud_server" "k8s_master" {
   ssh_keys    = [hcloud_ssh_key.default.id]
 }
 
+# Hetzner worker nodes
 resource "hcloud_server" "k8s_worker" {
   count       = 2
   name        = "${var.cluster_name}-worker-${count.index}"
@@ -20,6 +29,13 @@ resource "hcloud_server" "k8s_worker" {
   ssh_keys    = [hcloud_ssh_key.default.id]
 }
 
+# Output master IP
 output "master_ip" {
   value = hcloud_server.k8s_master.ipv4_address
+}
+
+# Optional: Output private key to save/use later
+output "private_key_pem" {
+  value     = tls_private_key.default.private_key_pem
+  sensitive = true
 }
